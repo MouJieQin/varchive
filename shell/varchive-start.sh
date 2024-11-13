6d9f12c52338b28a754b04a463a1f67477e82f5b
MouJieQin <724174637@qq.com>
2024-11-14 04:28:33 +0800

    Fix install.sh

--- shell/varchive-start.sh
+++ shell/varchive-start.sh
@@ -9,5 +9,8 @@ fi
 SCRIPT_DIR=$(dirname "$0")
 cd "$SCRIPT_DIR"
 nohup ./varchive-client.sh >>./varchive-client.log 2>&1 &
+cat "$SCRIPT_DIR/../spa/npm.log" | tail -n 7
+cd - >/dev/null
 cd "$SCRIPT_DIR/../server/src"
-nohup python3 varchive-server.py >>./server.log 2>&1 &
+nohup python3.9 varchive-server.py >>./server.log 2>&1 &
+cd - >/dev/null
