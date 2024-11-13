6cda496184cc52210ff3ce83fc959de527ef62e2
MouJieQin <724174637@qq.com>
2024-11-14 00:35:14 +0800

    Generate and install root CA using shell script

---
+++ server/pem/genCA.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Generate root certificate
+openssl genrsa -out rootCA.key 2048
+openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 3650 -out rootCA.pem -subj "/C=CA/ST=Province/L=City/O=Varchive/CN=Varchive Root CA"
+openssl genrsa -out server.key 2048
+openssl req -new -key server.key -out server.csr -subj "/C=CA/ST=Province/L=City/O=Varchive/CN=Varchive Root CA"
+openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 3650 -sha256 -extfile v3.ext
+openssl x509 -in rootCA.pem -out rootCA.crt -outform DER
+sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain rootCA.crt
