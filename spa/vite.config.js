9cea0062ee23b6658b83b78865fbbbd7920aa9c0
MouJieQin <724174637@qq.com>
2024-11-14 16:41:22 +0800

    Init electron project

--- spa/vite.config.js
+++ spa/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   resolve: {
     alias: {'@': fileURLToPath(new URL('./src', import.meta.url))},
   },
+  base: './',
   plugins: [vue()],
   server: {host: 'localhost', port: '5999'}
 })
